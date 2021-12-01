e610e9d5b6b6d25a5617c933c0ef01a45b6d2e60
Blasius Neri <blasius.neri@gmail.com>
2021-12-02 00:25:11 +0700

    Update Day01.kt
    improve code

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,7 +2,7 @@ import java.io.File
 
 fun main() {
     var increase = 0
-    val depths = readFile("src/aoc_2021_day1")
+    val depths = getListOfInt("src/aoc_2021_day1.txt")
     for(i in 1 until depths.size) {
         if (depths[i] > depths[i-1]) {
             increase++
@@ -11,6 +11,8 @@ fun main() {
     print(increase)
 }
 
-private fun readFile(filename: String): List<Int> = File(filename).useLines { it.toList() }.map {
+private fun getListOfInt(name: String): List<Int> = readInput("aoc_2021_day1").map {
     it.toInt()
 }
+
+private fun readInput(name: String) = File("src", "$name.txt").readLines()
